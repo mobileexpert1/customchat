2436a4068e710d5289814e2185ab901dddd2f549
apple <apple@CSPC178.local>
2021-01-20 20:07:00 +0530

    Temp Push

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require("socket.io");
+
 //mention public path
 const publicPath=path.join(__dirname,'/../public');
+
+//port app
 const port = process.env.PORT || 3000;
 
-var app=express();
+let app=express();
+let server=http.createServer(app);
+let io=socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+   console.log("here you go",socket);
+})
+
+server.listen(port,()=>{
     console.log(`server is up and running on port ${port}`);
 })
